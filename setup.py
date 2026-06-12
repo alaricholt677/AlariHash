379a716e6da874d3c3528027682f763c487085b2
alaricholt677 <alaricholt0@gmail.com>
2026-06-12 00:40:49 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alarihash",
+    version="1.0.0",
+    packages=find_packages(),
+    description="Python implementation of the AlariHash-X encoding pipeline",
+    author="alaricholt677",
+    url="https://github.com/alaricholt677/PKGS",
+)
